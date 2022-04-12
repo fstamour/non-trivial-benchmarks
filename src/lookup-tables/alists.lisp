1ad1fb05f2bff0455ee09a988799e20089ee4f39
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2022-04-11 20:19:22 -0600

    Funcitons are now exported

--- src/lookup-tables/alists.lisp
+++ src/lookup-tables/alists.lisp
@@ -1,32 +1,31 @@
-;;;; These benchmarks are not exported, so they're effectively
-;;;; disabled. I disabled them because they deadlock.
-
 (defpackage #:non-trivial-benchmarks/lookup-tables/alists  (:use :cl))
 
 (in-package #:non-trivial-benchmarks/lookup-tables/alists)
 
-(defun run-insertion-alist (&optional (n 1000000))
-  "Sequencially inserting values into an alist"
-  (declare (type fixnum n))
-  (let* ((ht '())
-         (random-list (loop :repeat n
-                            :for i = (random 100)
-                            :collect i)))
-    (benchmark:with-timing (n)
-      (loop :for i :in random-list
-            :do (push (cons i i) ht)))))
+(export
+ (defun run-insertion-alist (&optional (n 1000000))
+   "Sequencially inserting values into an alist"
+   (declare (type fixnum n))
+   (let* ((ht '())
+          (random-list (loop :repeat n
+                             :for i = (random 100)
+                             :collect i)))
+     (benchmark:with-timing (n)
+                            (loop :for i :in random-list
+                                  :do (push (cons i i) ht))))))
 
 
-(defun run-lookup-alist (&optional (n 1000000))
-  "Looking up values in an alist"
-  (declare (type fixnum n))
-  (let* ((ht '())
-         (random-list (loop :repeat n
-                            :for i = (random 100)
-                            :collect i)))
-    ;; Initialize Values
-    (loop :for i :in random-list
-          :do (push (cons i i) ht))
-    (benchmark:with-timing (n)
-      (loop :for i :in random-list
-            :do (assoc i ht)))))
+(export
+ (defun run-lookup-alist (&optional (n 1000000))
+   "Looking up values in an alist"
+   (declare (type fixnum n))
+   (let* ((ht '())
+          (random-list (loop :repeat n
+                             :for i = (random 100)
+                             :collect i)))
+     ;; Initialize Values
+     (loop :for i :in random-list
+           :do (push (cons i i) ht))
+     (benchmark:with-timing (n)
+                            (loop :for i :in random-list
+                                  :do (assoc i ht))))))
