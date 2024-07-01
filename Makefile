05803db9f22427bc983a43a346ca98c67356d151
Francis St-Amour <fr.stamour@gmail.com>
2024-07-01 00:14:35 -0400

    remove qlot

--- Makefile
+++ Makefile
@@ -1,14 +1,5 @@
 all: run
 
-# If dependencies are not installed attempt to install them using
-# the qlot executable
-.qlot/setup.lisp:
-	command -v qlot >/dev/null && qlot install
-
 .PHONY: run
-run: .qlot/setup.lisp
+run:
 	./scripts/run.sh
-
-.PHONY: clean
-clean:
-	rm -rf .qlot
--- scripts/run.sh
+++ scripts/run.sh
@@ -5,7 +5,15 @@
 # This script serves has documentation more than anything.
 #
 
-sbcl --load ".qlot/setup.lisp" \
+set -u -e
+
+cd "$(git rev-parse --show-toplevel)"
+
+sbcl --eval "(asdf:initialize-source-registry
+     \`(:source-registry
+        (:directory ,(truename \".\"))
+           :inherit-configuration))" \
+     --non-interactive \
      --eval '(ql:quickload :non-trivial-benchmarks)' \
      --eval '(non-trivial-benchmarks:run)' \
      --quit
